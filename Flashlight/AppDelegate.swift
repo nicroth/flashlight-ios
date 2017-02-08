22d1f5ae925f9bbe6d233cc44f5ce710ec9c865c
Nic Roth <nic.roth@gmail.com>
2017-02-08 16:37:06 -0600

    move all logic to the app delegate, automatically turns on and off now

--- Flashlight/AppDelegate.swift
+++ Flashlight/AppDelegate.swift
@@ -7,11 +7,50 @@
 //
 
 import UIKit
+import AVFoundation
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var captureDevice = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
 
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
+        turnTorchOn()
+        return true
+    }
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        turnTorchOn()
+    }
+    
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        turnTorchOn()
+    }
+    
+    func turnTorchOn() {
+        if captureDevice != nil && (captureDevice?.hasTorch)! {
+            do {
+                try captureDevice?.lockForConfiguration()
+                captureDevice?.torchMode = AVCaptureTorchMode.on
+                try captureDevice?.setTorchModeOnWithLevel(AVCaptureMaxAvailableTorchLevel)
+                captureDevice?.unlockForConfiguration()
+            } catch {
+                print(error)
+            }
+        }
+    }
+    
+    func turnTorchOff() {
+        if captureDevice != nil && (captureDevice?.hasTorch)! {
+            do {
+                try captureDevice?.lockForConfiguration()
+                captureDevice?.torchMode = AVCaptureTorchMode.off
+                captureDevice?.unlockForConfiguration()
+            } catch {
+                print(error)
+            }
+        }
+    }
 }
 
